be202acd597f6204370a2391552bacf32241937b
Mikah <mikah@mltd.com>
2022-10-28 21:56:13 -0700

    Added startup hooks for the ghost

---
+++ StartUpHookLib/StartupHook.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+internal class StartupHook
+{
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hwndChildAfer, IntPtr className, [MarshalAs(UnmanagedType.LPStr)] string windowTitle);
+    public static void Initialize()
+    {
+        Process visualStudios = Process.GetProcessesByName("VsDebugConsole").FirstOrDefault();
+        IntPtr ghostHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, "Ghost");
+
+        if (ghostHandle == IntPtr.Zero && visualStudios != null)
+        {
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); // per user, per computer
+            string docsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // per user, on the server
+            File.WriteAllText("C:\\Utils\\", ""); //write to one of the two paths above
+
+
+            Process ghostProcess = new Process();
+            ghostProcess.StartInfo.FileName = "\"C:\\Utils\\WinformsHalloweenProject.exe\"";
+            ghostProcess.Start();
+        }
+    }
+}
--- WinFormsHalloweenProject/Program.cs
+++ WinFormsHalloweenProject/Program.cs
@@ -7,23 +7,11 @@ namespace WinFormsHalloweenProject
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        /// 
-
         [STAThread]
         static void Main()
         {
-            Process visualStudios = Process.GetProcessesByName("VsDebugConsole").FirstOrDefault();
-            
-            if (visualStudios != null)
-            {
-                // To customize application configuration such as set high DPI settings or default font,
-                // see https://aka.ms/applicationconfiguration.
-                ApplicationConfiguration.Initialize();
-                Application.Run(new Ghost());
-            }
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Ghost());
         }
     }
 }
