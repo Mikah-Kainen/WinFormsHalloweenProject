ea3f00990d73f43b6701a50312a52f16d10d17e6
Mikah <mikah@mltd.com>
2022-10-29 20:30:36 -0700

    Added safegaurds to make sure only one ghost spawns from startup hooks

--- StartUpHookLib/StartupHook.cs
+++ StartUpHookLib/StartupHook.cs
@@ -3,22 +3,26 @@ using System.Runtime.InteropServices;
 
 internal class StartupHook
 {
-    [DllImport("user32.dll")]
-    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hwndChildAfer, IntPtr className, [MarshalAs(UnmanagedType.LPStr)] string windowTitle);
+    //[DllImport("user32.dll")]
+    //public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hwndChildAfer, IntPtr className, [MarshalAs(UnmanagedType.LPStr)] string windowTitle);
     public static void Initialize()
     {
         Process visualStudios = Process.GetProcessesByName("VsDebugConsole").FirstOrDefault();
-        IntPtr ghostHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, "Ghost");
+        //IntPtr ghostHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, "Ghost");
+        //string docsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // per user, on the server
+        string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); // per user, per computer
+        string filename = Path.Combine(userPath, "ExpectoPatronum");
 
-        if (ghostHandle == IntPtr.Zero && visualStudios != null)
+        bool doesFileExist = File.Exists(filename);
+
+        if (!doesFileExist && visualStudios != null)
         {
-            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); // per user, per computer
-            string docsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // per user, on the server
-            File.WriteAllText("C:\\Utils\\", ""); //write to one of the two paths above
 
+            Console.WriteLine("Spooky Spooky");
+            File.WriteAllText(filename, "");
 
             Process ghostProcess = new Process();
-            ghostProcess.StartInfo.FileName = "\"C:\\Utils\\WinformsHalloweenProject.exe\"";
+            ghostProcess.StartInfo.FileName = @"C:\Utils\WinformsHalloweenProject.exe";
             ghostProcess.Start();
         }
     }
